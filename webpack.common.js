37bf17359c8c63b235ff52466f5a8d15e4d3c333
malte <maltewallander1@gmail.com>
2024-05-29 20:13:01 +0200

    no js

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './js/app.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
   },
 };
